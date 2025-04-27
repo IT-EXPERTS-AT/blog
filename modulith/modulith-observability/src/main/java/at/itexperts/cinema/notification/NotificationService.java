3063f3266d4d22bf44c93c7b603d2e9e11755128
Tobias Haindl <tobias.haindl@it-experts.at>
2025-04-27 12:11:06 +0200

    Step 4: add @Scheduled example

--- modulith/modulith-observability/src/main/java/at/itexperts/cinema/notification/NotificationService.java
+++ modulith/modulith-observability/src/main/java/at/itexperts/cinema/notification/NotificationService.java
@@ -1,9 +1,11 @@
 package at.itexperts.cinema.notification;
 
 import static at.itexperts.cinema.generated.Sequences.NOTIFICATION_SEQ;
+import static org.jooq.impl.DSL.count;
 
 import at.itexperts.cinema.generated.Tables;
 import org.jooq.DSLContext;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,9 +19,15 @@ public class NotificationService {
         this.dsl = dsl;
     }
 
+    @Transactional
     public void addTicketPurchaseNotification() {
         dsl.insertInto(Tables.NOTIFICATION)
                 .values(NOTIFICATION_SEQ.nextval(), "TICKET")
                 .execute();
+    }
+
+    @Scheduled(fixedDelayString = "PT1M")
+    public void countTicketPurchaseNotification() {
+        dsl.select(count()).from(Tables.NOTIFICATION).execute();
     }
 }
