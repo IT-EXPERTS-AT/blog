82f1a11983019a3c80a9ee1b3891895038e5cf85
Tobias Haindl <tobias.haindl@it-experts.at>
2025-08-22 12:26:34 +0200

    Cleanup

--- datasource-proxy/datasource-proxy/src/test/java/at/itexperts/datasourceproxy/QueryTracker.java
+++ datasource-proxy/datasource-proxy/src/test/java/at/itexperts/datasourceproxy/QueryTracker.java
@@ -2,6 +2,8 @@ package at.itexperts.datasourceproxy;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -19,13 +21,20 @@ import org.assertj.core.api.AbstractLongAssert;
 
 public class QueryTracker extends DataSourceQueryCountListener {
 
-  private Set<QueryType> queryTypes;
-  private Map<QueryType, List<ParsedQuery>> queries;
+  private final Set<QueryType> queryTypes;
+  private final Map<QueryType, List<ParsedQuery>> queries;
 
   public QueryTracker(Set<QueryType> queryTypes) {
     setQueryCountStrategy(new SingleQueryCountHolder());
-    this.queryTypes = queryTypes;
+
+    this.queryTypes = Collections.synchronizedSet(new HashSet<>());
+    this.queryTypes.addAll(queryTypes);
+
     this.queries = new ConcurrentHashMap<>();
+  }
+
+  public void track(QueryType... typesToTrack) {
+    track(Set.of(typesToTrack));
   }
 
   public void track(Set<QueryType> typesToTrack) {
@@ -39,10 +48,6 @@ public class QueryTracker extends DataSourceQueryCountListener {
     this.queries.clear();
   }
 
-  public void track(QueryType... typesToTrack) {
-    track(Set.of(typesToTrack));
-  }
-
   @Override
   public void afterQuery(ExecutionInfo execInfo, List<QueryInfo> queryInfoList) {
     super.afterQuery(execInfo, queryInfoList);
@@ -51,7 +56,7 @@ public class QueryTracker extends DataSourceQueryCountListener {
       var type = QueryUtils.getQueryType(queryInfo.getQuery());
       if (queryTypes.contains(type)) {
         queries
-            .computeIfAbsent(type, k -> new java.util.concurrent.CopyOnWriteArrayList<>())
+            .computeIfAbsent(type, _ -> new java.util.concurrent.CopyOnWriteArrayList<>())
             .add(toParsedQuery(queryInfo));
       }
     }
